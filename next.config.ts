ce9e376b6e4c72a0048eb7b3066cbd57c7d005cb
Austin Falk <austin@austinfalk.com>
2025-08-13 15:12:36 -0700

    build(dependencies): merging deps from commerce

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: true,
+    inlineCss: true,
+    useCache: true,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/s/files/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
